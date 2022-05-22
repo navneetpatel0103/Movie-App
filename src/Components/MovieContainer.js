23790ca8b0f1f6ddeeede79bad00a333ac0e20fe
Navneet Patel <navneetpatel0103@gmail.com>
2022-05-23 01:13:48 +0530

    info container

--- src/Components/MovieContainer.js
+++ src/Components/MovieContainer.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import MovieInfo from './MovieInfo';
 
-export default function MovieContainer() {
+export default function MovieContainer(props) {
+  const {Title, Year, imdbID, Type, Poster}=props.movie;
+ 
   return (
-    <div style={{width:"310px", padding:"10px", boxShadow:"0 3px 10px 0 #aaa", cursor:"pointer"}}>
-       <img className='poster' src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" width="360px" height="395px" />
-       <div style={{padding:"10px 0", whiteSpace:"nowrap", overflow:"hidden", textOverflow:"ellipsis", fontWeight:"600"}}>Guardians of the Galaxy Vol. 2 </div>
+    <div style={{width:"270px", padding:"10px", boxShadow:"0 3px 10px 0 #aaa", cursor:"pointer"}} onClick={()=>{props.onclick(imdbID)}}>
+       <img className='poster' src={Poster} width="360px" height="400px" />
+       <div style={{padding:"10px 0", whiteSpace:"nowrap", overflow:"hidden", textOverflow:"ellipsis", fontWeight:"600"}}>{Title} </div>
        <div className="row">
-           <div className='col-5'>Date-2014</div>
-           <div className='col-5 offset-2'>Genre-Action</div>
+           <div className='col-5'>Year: {Year}</div>
+           <div className='col-5 offset-2'>Type: {Type}</div>
        </div>
+
     </div>
+    
   )
 }
---
+++ src/Components/MovieInfo.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import React, {useEffect,useState} from 'react'
+import cross from '../Assets/Images/cross.png'
+import { API_KEY } from '../App'
+export default function MovieInfo(props) {
+    const [movieInfo, setMovieInfo] = useState();
+    useEffect(() => {
+        axios.get(`https://www.omdbapi.com/?i=${props.selectedMovie}&apikey=${API_KEY}`)
+        .then((response)=>{
+            setMovieInfo(response.data);
+        })
+    }, [props.selectedMovie])
+    
+
+
+  return (
+    <div className='movieInfoContainer' >
+        {movieInfo?<>
+            <img className='poster' style={{margin:"0 25px"}} src={movieInfo?.Poster} />
+        <div className="movieInfo">
+            <div style={{fontSize:"20px", fontWeight:"600", margin:"0 0 12px 0" }}>Movie: {movieInfo?.Title}</div>
+            <div className='indInfoHeading'>IMDB Rating: <span className='infoHeading'>{movieInfo?.imdbRating}</span></div>
+            <div className='indInfoHeading'>Year: <span className='infoHeading'>{movieInfo?.Year}</span></div>
+            <div className='indInfoHeading'>Language: <span className='infoHeading'>{movieInfo?.Language}</span></div>
+            <div className='indInfoHeading'>Rated:  <span className='infoHeading'>{movieInfo?.Rated}</span></div>
+            <div className='indInfoHeading'>Released: <span className='infoHeading'>{movieInfo?.Released}</span></div>
+            <div className='indInfoHeading'>Runtime: <span className='infoHeading'> {movieInfo?.Runtime}</span></div>
+            <div className='indInfoHeading'>Genre: <span className='infoHeading'> {movieInfo?.Genre}</span></div>
+            <div className='indInfoHeading'>Director: <span className='infoHeading'>{movieInfo?.Director}</span></div>
+            <div className='indInfoHeading'>Actors: <span className='infoHeading'>{movieInfo?.Actors}</span></div>
+            <div className='indInfoHeading'>Plot: <span className='infoHeading'>{movieInfo?.Plot}</span></div>
+        </div>
+        <span>< img src={cross} width="15px" height="15px" style={{cursor:"pointer"}} onClick={()=>props.setSelectedMovie()}/></span>
+        </>:" Loading...."}
+       
+        </div>
+  )
+}
